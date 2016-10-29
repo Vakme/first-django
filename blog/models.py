807e78db7ad6e0d5d94cf6821afb27cafb2d337c
vakme <4bielak.aleksandra@gmail.com>
2016-10-29 17:17:54 +0200

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.utils import timezone
+
+
+class Comment(models.Model):
+    post_id = models.ForeignKey('Post')
+    author = models.CharField(max_length=100, null=False)
+    text = models.TextField()
+    published_date = models.DateTimeField(
+        default=timezone.now)
+
+    def __str__(self):
+        return self.text
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from blog.forms import PostForm, CommentForm
+from blog.models import Post, Comment
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date').reverse()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post_id=pk).order_by('published_date')
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.published_date = timezone.now()
+            comment.post_id = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
